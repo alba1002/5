402d081c7420e10d17a3d25dc7da1a899663eb1c
alba1002 <108678930+alba1002@users.noreply.github.com>
2022-07-17 00:08:00 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hero Buch = new Hero(100, 20, "fly");
+        Hero Killua = new Hero(100, 50);
+        Boss boss1 = new Boss();
+        boss1.sethealth(300);
+        boss1.setdamage(50);
+        boss1.setsuperPower("Fly");
+        boss1.settypeDefence("Field");
+        System.out.println(boss1.getdamage() + " " + boss1.gethealth() + " " + boss1.gettypeDefence() + " " + boss1.getsuperPower());
     }
 }
