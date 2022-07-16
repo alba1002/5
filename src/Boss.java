716f78cb104792875fee5d86b6be6e297677ffda
alba1002 <108678930+alba1002@users.noreply.github.com>
2022-07-17 00:07:10 +0600

    Update Boss.java

--- src/Boss.java
+++ src/Boss.java
@@ -1,2 +1,32 @@
-package PACKAGE_NAME;public class Boss {
+
+public class Boss {
+    private int health;
+    private int damage;
+    private String typeDefence;
+    private String superPower;
+    public int gethealth(){
+        return health;
+    }
+    public int getdamage(){
+        return damage;
+    }
+    public String gettypeDefence(){
+        return typeDefence;
+    }
+    public String getsuperPower(){
+        return superPower;
+    }
+    public void sethealth(int Health){
+        this.health = health;
+    }
+    public void setdamage(int Damage){
+        this.health = damage;
+    }
+    public void settypeDefence(String typeDefence){
+        this.typeDefence = typeDefence;
+    }
+    public void setsuperPower(String superPower){
+        this.superPower = superPower;
+    }
 }
+
