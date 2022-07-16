6fcac303633fe00d2c7678c6c9e4277f564b06ea
alba1002 <108678930+alba1002@users.noreply.github.com>
2022-07-17 00:07:34 +0600

    Update Hero.java

--- src/Hero.java
+++ src/Hero.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Hero {
+
+public class Hero {
+    private int Health;
+    private int Damage;
+    private String superPower;
+
+    public Hero(int Health, int Damage, String superPower) {
+    }
+
+    public Hero(int Health, int Damage) {
+
+    }
+    public int getHealth(){
+        return Health;
+    }
+    public int getDamage(){
+        return Damage;
+    }
+    public String getSuperPower(){
+        return superPower;
+    }
 }
+
